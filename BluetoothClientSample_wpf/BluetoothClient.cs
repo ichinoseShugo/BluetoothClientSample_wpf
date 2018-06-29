7e2f480ddf59f73a5cb328ccfbd48a13fc025790
ichinoseShugo <ichinose@srmtlab.org>
2018-06-29 10:46:01 +0900

    initial commit

---
+++ BluetoothClientSample_wpf/BluetoothClient.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+
+using System.Collections.ObjectModel;
+using Windows.UI.Core;
+
+
+using System.ComponentModel;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.Foundation;
+using System.Windows.Controls;
+
+
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace BluetoothClientSample_wpf
+{
+    //BluetoothClient(子機)側のサンプルコード
+    class BluetoothClient
+    {
+        private RfcommDeviceService ConnectService = null;
+        private StreamSocket ConnectSocket = null;
+        private BluetoothDevice bluetoothDevice;
+        #region Connect   
+        public async void Connect(RfcommDeviceDisplay deviceInfoDisp)
+        {
+
+            try
+            {
+                bluetoothDevice = await BluetoothDevice.FromIdAsync(deviceInfoDisp.Id);
+            }
+            catch (Exception ex)
+            {
+                //rootPage.NotifyUser(ex.Message, NotifyType.ErrorMessage);
+                return;
+            }
+            // If we were unable to get a valid Bluetooth device object,
+            // it's most likely because the user has specified that all unpaired devices
+            // should not be interacted with.
+
+            // This should return a list of uncached Bluetooth services (so if the server was not active when paired, it will still be detected by this call
+            Guid RfcommChatServiceUuid = Guid.Parse("17fcf242-f86d-4e35-805e-546ee3040b84");
+            var rfcommServices = await bluetoothDevice.GetRfcommServicesForIdAsync(
+                RfcommServiceId.FromUuid(RfcommChatServiceUuid), BluetoothCacheMode.Uncached);
+
+            if (rfcommServices.Services.Count > 0)
+            {
+                ConnectService = rfcommServices.Services[0];
+            }
+            else
+            {
+                return;
+            }
+
+            // Do various checks of the SDP record to make sure you are talking to a device that actually supports the Bluetooth Rfcomm Chat Service
+            UInt16 SdpServiceNameAttributeId = 0x100;
+            var attributes = await ConnectService.GetSdpRawAttributesAsync();
+            if (!attributes.ContainsKey(SdpServiceNameAttributeId))
+            {
+                MessageBox.Show("sdpAttributeがおかしい");
+                return;
+            }
+
+            byte SdpServiceNameAttributeType = (4 << 3) | 5;
+            var attributeReader = DataReader.FromBuffer(attributes[SdpServiceNameAttributeId]);
+            var attributeType = attributeReader.ReadByte();
+            if (attributeType != SdpServiceNameAttributeType)
+            {
+                MessageBox.Show("sdpNameAttributeがおかしい");
+                return;
+            }
+            var serviceNameLength = attributeReader.ReadByte();
+
+            lock (this) //lock構文、排他制御
+            {
+                ConnectSocket = new StreamSocket();
+            }
+
+            try
+            {
+                await ConnectSocket.ConnectAsync(ConnectService.ConnectionHostName, ConnectService.ConnectionServiceName);
+            }
+            catch (Exception ex) when ((uint)ex.HResult == 0x80072740) // WSAEADDRINUSE
+            {
+                MessageBox.Show("socket接続がおかしい");
+            }
+        }
+
+        #endregion
+
+        //接続切断命令
+        public void Disconnect()
+        {
+            if (ConnectService != null)
+            {
+                ConnectService.Dispose();
+                ConnectService = null;
+            }
+
+            if (ConnectSocket.InputStream != null)
+            {
+                ConnectSocket.InputStream.Dispose();
+            }
+
+            if (ConnectSocket.OutputStream != null)
+            {
+                ConnectSocket.OutputStream.Dispose();
+            }
+
+            if (ConnectSocket != null)
+            {
+                ConnectSocket.Dispose();
+                ConnectSocket = null;
+            }
+
+        }
+
+        public async void send()
+        {
+            // There's no need to send a zero length message
+            // Make sure that the connection is still up and there is a message to send
+            if (ConnectSocket != null)
+            {
+                //7文字(7バイト)のデータ
+                string data = "ABCDEFG";
+                //バイトデータの文字コードを変更(androidを想定してUTF8に変更しているが変更の必要があるかどうかは未実験、必要ないかも)
+                byte[] bytes = System.Text.Encoding.UTF8.GetBytes(data);
+                //OutputStreamに文字列を送信
+                await ConnectSocket.OutputStream.WriteAsync(bytes.AsBuffer());
+
+            }
+        }
+
+        public async void receive()
+        {
+            try
+            {
+                if (ConnectSocket != null)
+                {
+                    byte[] buffer = new byte[120];
+                    //InputStreamのデータを変数bufferに格納
+                    await ConnectSocket.InputStream.ReadAsync(buffer.AsBuffer(), 120, InputStreamOptions.Partial);
+                    //受信したbyteデータを文字列に変換
+                    string str = Encoding.GetEncoding("ASCII").GetString(buffer);
+                    MessageBox.Show("" + str);
+                }
+            }
+            catch (Exception ex)
+            {
+                lock (this)
+                {
+                    if (ConnectSocket == null)
+                    {
+                        // Do not print anything here -  the user closed the sock
+                    }
+                    else
+                    {
+                        Disconnect();
+                    }
+                }
+            }
+        }
+    }//class
+}//namespace
--- BluetoothClientSample_wpf/MainWindow.xaml.cs
+++ BluetoothClientSample_wpf/MainWindow.xaml.cs
@@ -1,17 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+
+using System.Collections.ObjectModel;
+using Windows.UI.Core;
+
+
+using System.ComponentModel;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.Foundation;
+using System.Windows.Controls;
 
 namespace BluetoothClientSample_wpf
 {
@@ -23,6 +30,313 @@ namespace BluetoothClientSample_wpf
         public MainWindow()
         {
             InitializeComponent();
+            ResultCollection = new ObservableCollection<RfcommDeviceDisplay>();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            //viewのリストと変数のリストを紐づける
+            resultsListView.DataContext = ResultCollection;
+        }
+
+        /// <summary>
+        /// Windowが閉じる時に呼び出されるイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void WindowClosing(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        //通信可能なデバイス一覧を表示するListViewを操作する変数
+        public ObservableCollection<RfcommDeviceDisplay> ResultCollection
+        {
+            get;
+            private set;
+        }
+
+        private DeviceWatcher deviceWatcher;
+        private BluetoothClient bluetoothClient = new BluetoothClient();
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            bluetoothClient.Send();
+        }
+
+        private async void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            bluetoothClient.Read();
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            bluetoothClient.Disconnect();
+            ResetMainUI();
+        }
+
+
+
+
+
+
+        //Bluetooth接続機能に関する部分
+        #region Connect   
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            //デバイス一覧からデバイスが選択されているか確認
+            if (resultsListView.SelectedItem == null)
+            {
+                MessageBox.Show("接続先デバイスが選択されてないよ");
+                return;
+            }
+
+            bluetoothClient.Connect(resultsListView.SelectedItem as RfcommDeviceDisplay);
+
+            StopWatcher();
+
+            ReadButton.IsEnabled = true;
+            SendButton.IsEnabled = true;
+            DisconnectButton.IsEnabled = true;
+        }
+
+        //接続候補のリストのアイテム選択時に発生するイベントハンドラ
+        private void ResultsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdatePairingButtons();
+        }
+
+        private void UpdatePairingButtons()
+        {
+            RfcommDeviceDisplay deviceDisp = (RfcommDeviceDisplay)resultsListView.SelectedItem;
+
+            if (null != deviceDisp)
+            {
+                ConnectButton.IsEnabled = true;
+            }
+            else
+            {
+                ConnectButton.IsEnabled = false;
+            }
+        }
+        #endregion
+
+        //接続可能なデバイス一覧の取得と表示UI操作に関する部分
+        #region DeviceWatcher  
+        private void EnumerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (deviceWatcher == null)
+            {
+                //リスト表示のUIの初期化処理
+                SetDeviceWatcherUI();
+                //デバイス一覧を取得してリストに表示する命令
+                StartUnpairedDeviceWatcher();
+            }
+            else
+            {
+                //リストの一覧表示をリセットする
+                ResetMainUI();
+            }
+
+        }
+        private void ResetMainUI()
+        {
+            EnumerateButton.Content = "Start";
+            ConnectButton.Visibility = Visibility.Visible;
+            resultsListView.Visibility = Visibility.Visible;
+            resultsListView.IsEnabled = true;
+
+            // Re-set device specific UX
+            //RequestAccessButton.Visibility = Visibility.Collapsed;
+            StopWatcher();
+        }
+
+        private void SetDeviceWatcherUI()
+        {
+            // Disable the button while we do async operations so the user can't Run twice.
+            EnumerateButton.Content = "Stop";
+            MessageBox.Show("EnumerateStart");
+            resultsListView.Visibility = Visibility.Visible;
+            resultsListView.IsEnabled = true;
+        }
+        private void StopWatcher()
+        {
+            if (null != deviceWatcher)
+            {
+                if ((DeviceWatcherStatus.Started == deviceWatcher.Status ||
+                     DeviceWatcherStatus.EnumerationCompleted == deviceWatcher.Status))
+                {
+                    deviceWatcher.Stop();
+                }
+                deviceWatcher = null;
+            }
+        }
+
+        private void StartUnpairedDeviceWatcher()
+        {
+
+            // Request additional properties
+            string[] requestedProperties = new string[] { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            deviceWatcher = DeviceInformation.CreateWatcher("(System.Devices.Aep.ProtocolId:=\"{e0cbf06c-cd8b-4647-bb8a-263b43f0f974}\")",
+                                                            requestedProperties,
+                                                            DeviceInformationKind.AssociationEndpoint);
+
+            //接続可能なデバイス候補が出現した際に呼び出されるイベントハンドラ
+            deviceWatcher.Added += new TypedEventHandler<DeviceWatcher, DeviceInformation>(async (watcher, deviceInfo) =>
+            {
+                // Since we have the collection databound to a UI element, we need to update the collection on the UI thread.
+                await Dispatcher.BeginInvoke(
+                 new Action(() =>
+                 {
+                     // Make sure device name isn't blank
+                     if (deviceInfo.Name != "")
+                     {
+                         ResultCollection.Add(new RfcommDeviceDisplay(deviceInfo));
+                     }
+                 }
+                ));
+            });
+            //デバイス候補が更新されるたびに呼び出されるイベントハンドラ
+            deviceWatcher.Updated += new TypedEventHandler<DeviceWatcher, DeviceInformationUpdate>(async (watcher, deviceInfoUpdate) =>
+            {
+
+                await Dispatcher.BeginInvoke(
+                new Action(() => {
+                    foreach (RfcommDeviceDisplay rfcommInfoDisp in ResultCollection)
+                    {
+                        if (rfcommInfoDisp.Id == deviceInfoUpdate.Id)
+                        {
+
+                            rfcommInfoDisp.Update(deviceInfoUpdate);
+                            break;
+                        }
+                    }
+                }
+                ));
+            });
+
+            /*
+             * デバイス一覧の表示が終了した際に呼び出されるイベントハンドラ（現段階では使用していない）
+            deviceWatcher.EnumerationCompleted += new TypedEventHandler<DeviceWatcher, Object>(async (watcher, obj) =>
+            {
+                await Dispatcher.BeginInvoke(
+                new Action(() => {
+           
+                }
+                ));
+            });
+            */
+
+            //一覧から削除された際に呼び出されるイベントハンドラ
+            deviceWatcher.Removed += new TypedEventHandler<DeviceWatcher, DeviceInformationUpdate>(async (watcher, deviceInfoUpdate) =>
+            {
+                // Since we have the collection databound to a UI element, we need to update the collection on the UI thread.
+                await Dispatcher.BeginInvoke(
+                new Action(() => {
+                    // Find the corresponding DeviceInformation in the collection and remove it
+                    foreach (RfcommDeviceDisplay rfcommInfoDisp in ResultCollection)
+                    {
+                        if (rfcommInfoDisp.Id == deviceInfoUpdate.Id)
+                        {
+                            ResultCollection.Remove(rfcommInfoDisp);
+                            break;
+                        }
+                    }
+                }));
+            });
+
+            //デバイス一覧の列挙が停止した際に呼び出されるイベントハンドラ
+            deviceWatcher.Stopped += new TypedEventHandler<DeviceWatcher, Object>(async (watcher, obj) =>
+            {
+                await Dispatcher.BeginInvoke(
+                new Action(() => {
+                    ResultCollection.Clear();
+                }));
+            });
+
+            deviceWatcher.Start();
+        }
+        #endregion
+
+
+
+    }
+
+    //接続先候補のリストを表すクラス
+    #region DeviceDisplay
+    public class RfcommDeviceDisplay : INotifyPropertyChanged
+    {
+        private DeviceInformation deviceInfo;
+
+        public RfcommDeviceDisplay(DeviceInformation deviceInfoIn)
+        {
+            deviceInfo = deviceInfoIn;
+        }
+
+        public DeviceInformation DeviceInformation
+        {
+            get
+            {
+                return deviceInfo;
+            }
+
+            private set
+            {
+                deviceInfo = value;
+            }
+        }
+
+        public string Id
+        {
+            get
+            {
+                return deviceInfo.Id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return deviceInfo.Name;
+            }
+        }
+
+
+        public void Update(DeviceInformationUpdate deviceInfoUpdate)
+        {
+
+            deviceInfo.Update(deviceInfoUpdate);
+            OnPropertyChanged("Name");
+            //デバイスサムネ画像はいらないのでコメントアウト
+            //UpdateGlyphBitmapImage();
+        }
+        /*
+        private async void UpdateGlyphBitmapImage()
+        {
+            DeviceThumbnail deviceThumbnail = await deviceInfo.GetGlyphThumbnailAsync();
+            BitmapImage glyphBitmapImage = new BitmapImage();
+            glyphBitmapImage.(deviceThumbnail);
+            GlyphBitmapImage = glyphBitmapImage;
+            OnPropertyChanged("GlyphBitmapImage");
+        }
+        */
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+
+                handler(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
+    #endregion
+
+
+
+
 }
